bd0474c07372288c11512ec78d8b4f65d12bae31
William Francis <william.francis@entelect.co.za>
2020-01-07 13:29:47 +0200

    finished with largest prime. can be optimized further, logic can be improved

--- Challenges/003/LargestPrimeFactor.js
+++ Challenges/003/LargestPrimeFactor.js
@@ -5,30 +5,40 @@ function largestPrimeFactor(number) {
     var primes = [2,3,5,7]
 
     if (primes.indexOf(number) !== -1){
-        console.log(number);
-        console.log("_____________________________________________");
         return number;
     }
 
-    console.log("sqrtNum = " +sqrtNum);
+    var largestPrime = 0;
 
-    for(var i = 0; i < sqrtNum+1; i++) {
-        console.log(i);
-        for(var j = 0; j < i; j++){
-            if (i%j === 0){
-                console.log(j);
+    for(var i = 8; i < sqrtNum + 1; i++) {
+
+        var isPrime = true;
+
+        for(var j = 0; j < primes.length; j++) {
+
+            if(i % primes[j] === 0){
+
+                isPrime = false;
+                break;
             }
         }
 
+        if(isPrime){
+            primes.push(i);
+            if(number % i === 0){
+                largestPrime = i;
+            }
+        }
     }
-
+    
+    return largestPrime;
 }
 
 
 
-largestPrimeFactor(2);
-largestPrimeFactor(3);
-largestPrimeFactor(5);
-largestPrimeFactor(7);
-largestPrimeFactor(13195);
-//largestPrimeFactor(600851475143);
+// largestPrimeFactor(2);
+// largestPrimeFactor(3);
+// largestPrimeFactor(5);
+// largestPrimeFactor(7);
+//largestPrimeFactor(13195);
+largestPrimeFactor(600851475143);
